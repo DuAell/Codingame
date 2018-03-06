31449dcfb6d1968c1c29b15ade07b755723d9091
Emmanuel Ruhnau <duaell@gmail.com>
2018-03-06 19:53:03 +0100

    First commit

---
+++ Codingame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codingame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
