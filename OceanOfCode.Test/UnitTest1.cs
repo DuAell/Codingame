7766b63401314608eb931446a6d3a4d0a9e0b2a0
Emmanuel Ruhnau <duaell@gmail.com>
2020-03-22 02:38:58 +0100

    Updating the possible ennemy positions map following his moves

--- OceanOfCode.Test/UnitTest1.cs
+++ OceanOfCode.Test/UnitTest1.cs
@@ -7,12 +7,16 @@ namespace OceanOfCode.Test
         [SetUp]
         public void Setup()
         {
+
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            var initData = "15 15 0§.....xx........§...............§...............§...xxx.........§...xxx.........§...xxx.........§.xx.xx.........§.xx............§....xxx........§....xxx........§.....xx........§.....xx...xx...§..........xx...§xx..xx.........§xx..xx.........";
+            var turnData = "10 14 6 6 3 -1 -1 -1§NA§NA§10 13 6 6 2 -1 -1 -1§NA§MOVE W§11 13 6 6 1 -1 -1 -1§NA§MOVE N§12 13 6 6 0 -1 -1 -1§NA§MOVE W§12 12 5 6 3 -1 -1 -1§NA§MOVE W§12 11 5 6 2 -1 -1 -1§NA§TORPEDO 2 0|MOVE W§12 10 5 6 1 -1 -1 -1§NA§MOVE W§12 9 5 6 0 -1 -1 -1§NA§MOVE N§12 8 5 5 3 -1 -1 -1§NA§TORPEDO 2 0|MOVE N§12 7 5 5 2 -1 -1 -1§NA§MOVE E§12 6 5 5 1 -1 -1 -1§NA§MOVE E§12 5 5 5 0 -1 -1 -1§NA§MOVE S";
+            var startPositionData = "10 4";
+            Game.Play(initData, turnData, startPositionData);
         }
     }
 }
--- OceanOfCode/Program.cs
+++ OceanOfCode/Program.cs
@@ -25,20 +25,25 @@ namespace OceanOfCode
 
         public static Player Ennemy { get; set; }
 
-        public static void Play(string initData = null, string turnData = null)
+        public static void Play(string initData = null, string turnData = null, string startPositionData = null)
         {
             var initDataInputProcessor = new InputProcessor(initData);
+            var turnDataInputProcessor = new InputProcessor(turnData);
             Initialize(initDataInputProcessor);
 
             // Output starting position
-            var startingPosition = Map.Tiles.Where(x => x.IsWater).OrderBy(x => Guid.NewGuid()).First();
-            startingPosition = Map.Tiles.First(_ => _.X == 4 && _.Y == 5);
+            var startingPosition = Map.Tiles.Where(x => x.IsWater).OrderBy(x => Guid.NewGuid()).First().ToString();
+            if (startPositionData != null)
+                startingPosition = startPositionData;
+
+            //startingPosition = "11 8";
+
             Console.WriteLine(startingPosition);
 
             while (true)
             {
                 initDataInputProcessor.WriteDebugData();
-                GameTurn(turnData);
+                GameTurn(turnDataInputProcessor);
             }
         }
 
@@ -69,10 +74,8 @@ namespace OceanOfCode
             };
         }
 
-        public static void GameTurn(string turnData = null)
+        public static void GameTurn(InputProcessor inputProcessor)
         {
-            var inputProcessor = new InputProcessor(turnData);
-
             var inputs = inputProcessor.ReadLine().Split(' ');
             Me.X = int.Parse(inputs[0]);
             Me.Y = int.Parse(inputs[1]);
@@ -88,19 +91,130 @@ namespace OceanOfCode
 
             inputProcessor.WriteDebugData();
 
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
+            UpdateEnnemyPosition(opponentOrders);
 
+            ShowMapOfPossibleEnnemyPositions();
+
+            var outputs = new List<string>();
             if (Me.CanGo(Direction.North))
-                Console.WriteLine("MOVE N TORPEDO");
+                outputs.Add("MOVE N TORPEDO");
             else if (Me.CanGo(Direction.Est))
-                Console.WriteLine("MOVE E TORPEDO");
+                outputs.Add("MOVE E TORPEDO");
             else if (Me.CanGo(Direction.South))
-                Console.WriteLine("MOVE S TORPEDO");
+                outputs.Add("MOVE S TORPEDO");
             else if (Me.CanGo(Direction.West))
-                Console.WriteLine("MOVE W TORPEDO");
-            else 
-                Console.WriteLine("SURFACE");
+                outputs.Add("MOVE W TORPEDO");
+            else
+            {
+                outputs.Add("SURFACE");
+                Me.TileInfos.ForEach(_ => _.HasBeenVisited = false);
+            }
+
+            var ennemyPossiblePositions = Ennemy.TileInfos.Where(_ => _.CanBeThere).ToList();
+
+            if (torpedoCooldown <= 0 && ennemyPossiblePositions.Count < 10)
+            {
+                // No matter if I shot myself, I will kill him
+                if (ennemyPossiblePositions.Count == 1 && Me.Life > 1 && Ennemy.Life <= 2 && ennemyPossiblePositions.First().Tile.Manhattan(Me) <= 4)
+                {
+                    outputs.Add($"TORPEDO {Ennemy.TileInfos.First(_ => _.CanBeThere).Tile}");
+                }
+                else
+                {
+                    var fireTarget = ennemyPossiblePositions.Where(_ => _.Tile.Manhattan(Me) <= 4 && _.Tile.Manhattan(Me) > 2).OrderBy(_ => Guid.NewGuid()).FirstOrDefault();
+                    if (fireTarget != null)
+                        outputs.Add($"TORPEDO {fireTarget.Tile}");
+                }
+            }
+
+            Console.WriteLine(string.Join(" | ", outputs));
+        }
+
+        private static void ShowMapOfPossibleEnnemyPositions()
+        {
+            for (var row = 0; row <= Ennemy.TileInfos.Max(_ => _.Tile.Y); row++)
+            {
+                var line = string.Empty;
+                for (var column = 0; column <= Ennemy.TileInfos.Max(_ => _.Tile.X); column++)
+                {
+                    var tileInfo = Ennemy.GetTileInfo(Map.Tiles.First(_ => _.X == column && _.Y == row));
+                    if (!tileInfo.Tile.IsWater)
+                        line += "x";
+                    else if (tileInfo.CanBeThere)
+                        line += "?";
+                    else
+                        line += ".";
+                }
+
+                Console.Error.WriteLine(line);
+            }
+        }
+
+        private static void UpdateEnnemyPosition(string opponentOrders)
+        {
+            var orders = opponentOrders.Split('|').Select(_ => _.Trim());
+            foreach (var order in orders)
+            {
+                if (order.StartsWith("MOVE "))
+                {
+                    UpdateEnnemyPosition_MoveTheMap(GetDirection(order.Substring(5, 1)));
+                }
+            }
+
+            Console.Error.WriteLine($"Ennemy can be in {Ennemy.TileInfos.Count(_ => _.CanBeThere)} positions");
+        }
+
+        private static Direction GetDirection(string letter)
+        {
+            switch (letter)
+            {
+                case "N":
+                    return Direction.North;
+                case "S":
+                    return Direction.South;
+                case "E":
+                    return Direction.Est;
+                case "W":
+                    return Direction.West;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(letter));
+            }
+        }
+
+        private static void UpdateEnnemyPosition_MoveTheMap(Direction direction)
+        {
+            Direction oppositeDirection = Direction.North; // Default stupid value
+            switch (direction)
+            {
+                case Direction.North:
+                    oppositeDirection = Direction.South;
+                    break;
+                case Direction.West:
+                    oppositeDirection = Direction.Est;
+                    break;
+                case Direction.Est:
+                    oppositeDirection = Direction.West;
+                    break;
+                case Direction.South:
+                    oppositeDirection = Direction.North;
+                    break;
+            }
+
+            var canBeThereToFalse = new List<TileInfo>();
+            var canBeThereToTrue = new List<TileInfo>();
+
+            // Update all tiles with an adjacent of opposite direction with CanBeThere false to false => If going north : Remove bottom line
+            canBeThereToFalse = Ennemy.TileInfos.Where(_ =>
+                _.CanBeThere &&
+                Ennemy.GetTileInfo(Map.GetAdjacent(_.Tile, oppositeDirection))?.CanBeThere != true).ToList();
+
+            // Update all tiles with an adjacent of opposite direction with CanBeThere true to true => If going north : Add upper line
+            canBeThereToTrue = Ennemy.TileInfos.Where(_ => !_.CanBeThere &&
+                                                           _.Tile.IsWater &&
+                                                           Ennemy.GetTileInfo(Map.GetAdjacent(_.Tile, oppositeDirection))?.CanBeThere == true).ToList();
+            
+            canBeThereToFalse.ForEach(_ => _.CanBeThere = false);
+            canBeThereToTrue.ForEach(_ => _.CanBeThere = true);
         }
     }
 
@@ -120,10 +234,7 @@ namespace OceanOfCode
 
         public bool CanGo(Direction direction)
         {
-            var xModifier = direction == Direction.Est ? 1 : direction == Direction.West ? -1 : 0;
-            var yModifier = direction == Direction.South ? 1 : direction == Direction.North ? -1 : 0;
-
-            var expectedDestination = Game.Map.Tiles.FirstOrDefault(_ => _.X == X + xModifier && _.Y == Y + yModifier);
+            var expectedDestination = Game.Map.GetAdjacent(this, direction);
             var result = expectedDestination?.IsWater == true && !GetTileInfo(expectedDestination).HasBeenVisited;
 
             Console.Error.WriteLine($"{(result ? "Can" : "Cannot")} go {direction} ({expectedDestination}). Water: {expectedDestination?.IsWater} / Already visited: {GetTileInfo(expectedDestination)?.HasBeenVisited}");
@@ -138,6 +249,39 @@ namespace OceanOfCode
         public int Height { get; set; }
 
         public List<Tile> Tiles { get; set; } = new List<Tile>();
+
+        public Tile GetAdjacent(Position position, Direction direction)
+        {
+            int xModifier;
+            switch (direction)
+            {
+                case Direction.Est:
+                    xModifier = 1;
+                    break;
+                case Direction.West:
+                    xModifier = -1;
+                    break;
+                default:
+                    xModifier = 0;
+                    break;
+            }
+
+            int yModifier;
+            switch (direction)
+            {
+                case Direction.South:
+                    yModifier = 1;
+                    break;
+                case Direction.North:
+                    yModifier = -1;
+                    break;
+                default:
+                    yModifier = 0;
+                    break;
+            }
+
+            return Tiles.FirstOrDefault(_ => _.X == position.X + xModifier && _.Y == position.Y + yModifier);
+        }
     }
 
     public class TileInfo
@@ -153,7 +297,7 @@ namespace OceanOfCode
 
         public bool HasBeenVisited { get; set; }
 
-        public bool CanBeThere { get; set; }
+        public bool CanBeThere { get; set; } = true;
     }
 
     public enum Direction
