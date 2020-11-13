10168a1b08caeee9b85955398dd3a53e259949d2
Emmanuel Ruhnau <eruhnau@sii.fr>
2020-11-13 01:02:15 +0100

    Add spells

--- FallChallenge2020/Program.cs
+++ FallChallenge2020/Program.cs
@@ -23,6 +23,7 @@ public class Game
     public InputProcessor InputProcessor { get; private set; }
 
     public List<Order> Orders { get; set; }
+    public List<Spell> Spells { get; set; }
     public List<Player> Players { get; set; }
     public Player Me => Players[0];
     public Player Opponent => Players[1];
@@ -43,57 +44,75 @@ public class Game
         string[] inputs;
         var actionCount = int.Parse(InputProcessor.ReadLine()); // the number of spells and recipes in play
         Orders = new List<Order>();
+        Spells = new List<Spell>();
         for (var i = 0; i < actionCount; i++)
         {
             inputs = InputProcessor.ReadLine().Split(' ');
-            Orders.Add(new Order
+
+            switch (inputs[1])
             {
-                Id = int.Parse(inputs[0]), // the unique ID of this spell or recipe
-                ActionType = inputs[1], // in the first league: BREW; later: CAST, OPPONENT_CAST, LEARN, BREW
-                Delta0 = int.Parse(inputs[2]), // tier-0 ingredient change
-                Delta1 = int.Parse(inputs[3]), // tier-1 ingredient change
-                Delta2 = int.Parse(inputs[4]), // tier-2 ingredient change
-                Delta3 = int.Parse(inputs[5]), // tier-3 ingredient change
-                Price = int.Parse(inputs[6]), // the price in rupees if this is a potion
-                TomeIndex = int.Parse(inputs[7]), // in the first two leagues: always 0; later: the index in the tome if this is a tome spell, equal to the read-ahead tax
-                TaxCount = int.Parse(inputs[8]), // in the first two leagues: always 0; later: the amount of taxed tier-0 ingredients you gain from learning this spell
-                Castable = inputs[9] != "0", // in the first league: always 0; later: 1 if this is a castable player spell
-                Repeatable = inputs[10] != "0" // for the first two leagues: always 0; later: 1 if this is a repeatable player spell
-            });
+                case "BREW":
+                    var order = new Order();
+                    order.Create(inputs);
+                    Orders.Add(order);
+                    break;
+                case "CAST":
+                    var spell = new Spell();
+                    spell.Create(inputs);
+                    Spells.Add(spell);
+                    break;
+            }
         }
 
         Players = new List<Player>();
         for (var i = 0; i < 2; i++)
         {
             inputs = InputProcessor.ReadLine().Split(' ');
-            Players.Add(new Player
+            var player = new Player
             {
-                Inv0 = int.Parse(inputs[0]), // tier-0 ingredients in inventory
-                Inv1 = int.Parse(inputs[1]),
-                Inv2 = int.Parse(inputs[2]),
-                Inv3 = int.Parse(inputs[3]),
-                Score = int.Parse(inputs[4]) // amount of rupees
-            });
+                Score = int.Parse(inputs[4])// amount of rupees;
+            };
+            Players.Add(player);
+            player.Inventory.Inv0 = int.Parse(inputs[0]); // tier-0 ingredients in inventory
+            player.Inventory.Inv1 = int.Parse(inputs[1]);
+            player.Inventory.Inv2 = int.Parse(inputs[2]);
+            player.Inventory.Inv3 = int.Parse(inputs[3]);
         }
 
         InputProcessor.WriteDebugData();
 
         foreach (var order in Orders.OrderByDescending(_ => _.Price))
         {
-            Console.Error.WriteLine($"Processing order {order.Id}");
-            if (Me.CanBrew(order))
+            if (order.CanUse(Me.Inventory))
             {
                 Console.WriteLine($"BREW {order.Id}");
                 return;
             }
         }
 
+        foreach (var spell in Spells.Where(_ => _.Castable && _.CanUse(Me.Inventory)))
+        {
+            var inventory = Me.Inventory.Clone();
+            spell.Use(inventory);
+            if (Orders.Any(_ => _.CanUse(inventory)))
+            {
+                Console.WriteLine($"CAST {spell.Id} Will brew");
+                return;
+            }
+        }
+
+        foreach (var spell in Spells.Where(_ => _.Castable && _.CanUse(Me.Inventory)))
+        {
+            Console.WriteLine($"CAST {spell.Id} Casting for fun");
+            return;
+        }
+        
         // in the first league: BREW <id> | WAIT; later: BREW <id> | CAST <id> [<times>] | LEARN <id> | REST | WAIT
         Console.WriteLine("REST");
     }
 }
 
-public class Order
+public abstract class AbstractAction
 {
     public int Id { get; set; }
     public string ActionType { get; set; }
@@ -106,22 +125,78 @@ public class Order
     public int TaxCount { get; set; }
     public bool Castable { get; set; }
     public bool Repeatable { get; set; }
+
+    public void Create(string[] inputs)
+    {
+        Id = int.Parse(inputs[0]); // the unique ID of this spell or recipe
+        ActionType = inputs[1]; // in the first league: BREW; later: CAST, OPPONENT_CAST, LEARN, BREW
+        Delta0 = int.Parse(inputs[2]); // tier-0 ingredient change
+        Delta1 = int.Parse(inputs[3]); // tier-1 ingredient change
+        Delta2 = int.Parse(inputs[4]); // tier-2 ingredient change
+        Delta3 = int.Parse(inputs[5]); // tier-3 ingredient change
+        Price = int.Parse(inputs[6]); // the price in rupees if this is a potion
+        TomeIndex = int.Parse(inputs[7]); // in the first two leagues: always 0; later: the index in the tome if this is a tome spell, equal to the read-ahead tax
+        TaxCount = int.Parse(inputs[8]); // in the first two leagues: always 0; later: the amount of taxed tier-0 ingredients you gain from learning this spell
+        Castable = inputs[9] != "0"; // in the first league: always 0; later: 1 if this is a castable player spell
+        Repeatable = inputs[10] != "0"; // for the first two leagues: always 0; later: 1 if this is a repeatable player spell
+    }
+
+
+}
+
+public class DefaultAction : AbstractAction
+{
+    
+}
+
+public class Order : AbstractAction
+{
+    public bool CanUse(Inventory inventory)
+    {
+        return inventory.Inv0 + Delta0 >= 0 &&
+               inventory.Inv1 + Delta1 >= 0 &&
+               inventory.Inv2 + Delta2 >= 0 &&
+               inventory.Inv3 + Delta3 >= 0;
+    }
+}
+
+public class Spell : AbstractAction
+{
+    public bool CanUse(Inventory inventory)
+    {
+        var inventoryCloned = inventory.Clone();
+        Use(inventoryCloned);
+        return inventoryCloned.Size <= 10;
+    }
+
+    public void Use(Inventory inventory)
+    {
+        inventory.Inv0 += Delta0;
+        inventory.Inv1 += Delta1;
+        inventory.Inv2 += Delta2;
+        inventory.Inv3 += Delta3;
+    }
 }
 
 public class Player
+{
+    public Inventory Inventory { get; set; } = new Inventory();
+
+    public int Score { get; set; }
+}
+
+public class Inventory
 {
     public int Inv0 { get; set; }
     public int Inv1 { get; set; }
     public int Inv2 { get; set; }
     public int Inv3 { get; set; }
-    public int Score { get; set; }
 
-    public bool CanBrew(Order order)
+    public int Size => Inv0 + Inv1 + Inv2 + Inv3;
+
+    public Inventory Clone()
     {
-        return Inv0 + order.Delta0 >= 0 && 
-               Inv1 + order.Delta1 >= 0 && 
-               Inv2 + order.Delta2 >= 0 &&
-               Inv3 + order.Delta3 >= 0;
+        return (Inventory) MemberwiseClone();
     }
 }
 
@@ -155,6 +230,7 @@ public class InputProcessor
 
     public void WriteDebugData()
     {
-        Console.Error.WriteLine(string.Join("§", _readLines));
+        _readLines.ForEach(Console.Error.WriteLine);
+        //Console.Error.WriteLine(string.Join("§", _readLines));
     }
 }
